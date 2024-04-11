3bf6c7c0b6f43bba1a8555fc671a03915e9cd930
Zane Bookbinder <zane.bookbinder@gmail.com>
2024-04-11 14:56:35 -0400

    Added data

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fml-nba-betting
+
+## The data in kaggle_data is pulled from: https://www.kaggle.com/datasets/erichqiu/nba-odds-and-scores?resource=download
--- betting_data_scraper.py
+++ betting_data_scraper.py
@@ -8,23 +8,46 @@ Created on Wed Apr 10 20:42:33 2024
 
 import requests
 import pandas as pd
+from bs4 import BeautifulSoup
 
+class OddsDataScraper():
+	# def __init__(self):
+	# 	return
+	
+    def scrape_rotowire_odds(self, url='https://www.rotowire.com/betting/nba/tables/games-archive.php'):
+        # Fetch the webpage content
+        r = requests.get(url)
+        data = r.json()
 
-# URL of the page containing NBA game spreads
-url = 'https://www.rotowire.com/betting/nba/tables/games-archive.php'
+        # Read the json data into a pandas df
+        df = pd.DataFrame(data, columns=['game_date', 'home_team_abbrev', 'visit_team_abbrev',
+                                        'line', 'home_team_score', 'visit_team_score'])
+        df['Final Score Spread'] = df['visit_team_score'] - df['home_team_score']
+        df = df.sort_values(by='home_team_abbrev')
+        df = df.rename(columns={'home_team_abbrev': 'Home Team', 'game_date': 'Date',
+                                'visit_team_abbrev': 'Away Team', 'line': 'Betting Score Spread'})
+        df = df.drop(columns=['home_team_score', 'visit_team_score'])
+        df['Betting Line Error'] = df['Betting Score Spread'] - df['Final Score Spread']
+        df.to_csv('nba_betting_lines.csv')
 
-# Fetch the webpage content
-r = requests.get(url)
-data = r.json()
+    def scrape_sportsbookreviews_online_odds(self, url='https://www.sportsbookreviewsonline.com/scoresoddsarchives/nba-odds-'):
+        df = pd.DataFrame(columns=['game_date', 'home_team_abbrev', 'visit_team_abbrev',
+                                        'line', 'home_team_score', 'visit_team_score'])
+        
 
+        for year in range(2007, 2023):
+            next_year = year + 1
+            url_ending = str(year) + '-' + str(next_year)[2:]
+            r = requests.get(url + url_ending)
+            print(url + url_ending)
+            soup = BeautifulSoup(r.content, 'html5lib')
+            print(soup)
+        # /nba-odds-2007-08/
 
-# Read the json data into a pandas df
-df = pd.DataFrame(data, columns=['game_date', 'home_team_abbrev', 'visit_team_abbrev',
-                                 'line', 'home_team_score', 'visit_team_score'])
-df['Final Score Spread'] = df['visit_team_score'] - df['home_team_score']
-df = df.sort_values(by='home_team_abbrev')
-df = df.rename(columns={'home_team_abbrev': 'Home Team', 'game_date': 'Date',
-                        'visit_team_abbrev': 'Away Team', 'line': 'Betting Score Spread'})
-df = df.drop(columns=['home_team_score', 'visit_team_score'])
-df['Betting Line Error'] = df['Betting Score Spread'] - df['Final Score Spread']
-df.to_csv('nba_betting_lines.csv')
+def main():
+    scraper = OddsDataScraper()
+    # scraper.scrape_rotowire_odds()
+    scraper.scrape_sportsbookreviews_online_odds()
+
+if __name__ == '__main__':
+    main()
